719e788d4b1c4d098ac0025a97749990869e232f
ZeyangSun <sunzeyang0702@gmail.com>
2018-12-04 13:25:08 +0100

    new API come support search by productNo,productName and brand

---
+++ src/main/java/se/agilecourse/model/Company.java
@@ -0,0 +1,93 @@
+package se.agilecourse.model;
+
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+import se.agilecourse.annotation.CascadeSave;
+
+import java.util.List;
+
+@Document(collection="companies")
+
+public class Company{
+
+
+
+    private String id;
+
+    private String name;
+
+    private String description;
+
+
+
+    @DBRef
+
+    @CascadeSave
+
+    private List<Product> products;
+
+
+
+    public String getId() {
+
+        return id;
+
+    }
+
+
+
+    public void setId(String id) {
+
+        this.id = id;
+
+    }
+
+
+
+    public String getName() {
+
+        return name;
+
+    }
+
+
+
+    public void setName(String name) {
+
+        this.name = name;
+
+    }
+
+
+
+    public String getDescription() {
+
+        return description;
+
+    }
+
+
+
+    public void setDescription(String description) {
+
+        this.description = description;
+
+    }
+
+
+
+    public List<Product> getProducts() {
+
+        return products;
+
+    }
+
+
+
+    public void setProducts(List<Product> products) {
+
+        this.products = products;
+
+    }
+
+}
---
+++ src/main/java/se/agilecourse/repository/CompanyRepository.java
@@ -0,0 +1,12 @@
+package se.agilecourse.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import se.agilecourse.model.Company;
+
+import java.util.Optional;
+
+public interface CompanyRepository extends MongoRepository<Company, String>, CustomizedRepository {
+
+    Optional<Company> findById(String id);
+
+}
