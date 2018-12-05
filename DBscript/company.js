9ef6b1f53878710c87d52bdd2edbe1adb47042eb
junaidrz46 <jr223ac@student.lnu.se>
2018-12-05 14:19:07 +0100

    write the script to restore database and upload files into matierials

---
+++ DBscript/company.js
@@ -0,0 +1,14 @@
+db.companies.insertMany([
+     {
+         "name":"Apple"
+     },
+    {
+        "name":"HUAWEI"
+    },
+    {
+        "name":"LG"
+    },
+    {
+        "name":"Samsung"
+    }
+ ]);
