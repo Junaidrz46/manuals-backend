7c77c3324f75251b42378a123124aae731b206ac
junaidrz46 <jr223ac@student.lnu.se>
2019-01-17 10:48:40 +0100

    updated the json file for postmen

--- src/main/java/se/agilecourse/controller/UserController.java
+++ src/main/java/se/agilecourse/controller/UserController.java
@@ -145,7 +145,8 @@ public class UserController {
        return emailService.sendEmail(emailMessage.getRecipients(),emailMessage.getSubject(),emailMessage.getEmailBody());
     }
 
-    @RequestMapping(value = "/sendEmailtoOptInUsers", method = RequestMethod.GET)
+    //@RequestMapping(value = "/sendEmailtoOptInUsers", method = RequestMethod.GET)
+    @PostMapping("/sendEmailtoOptInUsers")
     public String sendEmailMessaetoOptInUsers(@RequestBody WrapperEmailMessage emailMessage){
         return emailService.sendEmailtoOptInUsers(emailMessage.getSubject(),emailMessage.getEmailBody()) ;
     }
@@ -166,8 +167,10 @@ public class UserController {
         return userLikedProudctsService.findUsersEmailsSubscribedAndLikedCompany(companyId);
     }
 
-    @RequestMapping(value = "/SendEmailtoSubscribedUsersByCompanyId", method = RequestMethod.GET)
+    //@RequestMapping(value = "/SendEmailtoSubscribedUsersByCompanyId", method = RequestMethod.GET)
+    @PostMapping("/SendEmailtoSubscribedUsersByCompanyId")
     String sendSubscribedUsersEmailByCompanyId(@RequestBody WrapperEmailRequest emailRequest) {
+
        return emailService.sendEmail(
                 userLikedProudctsService.findUsersEmailsSubscribedAndLikedCompany(emailRequest.getCompanyId()),
                         emailRequest.getSubject(),emailRequest.getEmailBody());
--- src/main/java/se/agilecourse/model/User.java
+++ src/main/java/se/agilecourse/model/User.java
@@ -156,6 +156,14 @@ public class User {
         this.likedCompanies = likedCompanies;
     }
 
+    public String getTeleNumber() {
+        return teleNumber;
+    }
+
+    public void setTeleNumber(String teleNumber) {
+        this.teleNumber = teleNumber;
+    }
+
     @Override
     public String toString() {
 
